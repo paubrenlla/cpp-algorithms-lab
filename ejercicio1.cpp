52db9d04de91278a2d5b53ff092a6b6a495f619f
paubrenlla <pau.brenlla@gmail.com>
2024-09-27 23:14:18 -0300

    feat: elimine AVL.cpp e hice ejercicio1.cpp falta testear

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,8 +1,7 @@
 #include <cassert>
 #include <string>
 #include <iostream>
-#include <limits>
-#include "AVL.cpp"
+#include <climits>
 
 using namespace std;
 
@@ -14,108 +13,221 @@ struct Libro
 
     //Constructor
     Libro(int unId, string unTitulo): id(unId), titulo(unTitulo), habilitado(true) {}
-
-    // Sobrecarga del operador ==
-    bool operator==(const Libro& otro) const 
-    {
-        return id == otro.id;
-    }
-
-    // Sobrecarga del operador !=
-    bool operator!=(const Libro& otro) const 
-    {
-        return !(*this == otro);
-    }
-};
-
-class Biblioteca {
-    public:
-        Biblioteca() 
-        {
-            this->libros = new AVL<Libro>();
-            this->cantLibros = 0;
-            this->cantHabilitados = 0;
-        }
-
-        void agregar(int unId, int unTitulo){
-            Libro* busqueda = this->libros->contains(unId);
-            if (busqueda == NULL)
-            {
-                Libro nuevoLibro = new Libro(unId, unTitulo);
-                this->libros->insert(nuevoLibro);
-                this->cantHabilitados++;
-                this->cantLibros++;
-            } 
-            else
-            {
-                busqueda->titulo = unTitulo;
-                if (busqueda->habilitado == false)
-                {
-                    this->cantHabilitados++;
-                    busqueda->habilitado = true;
-                }
-            }
-        }
-
-        string buscar(int unId)
-        {
-            Libro* busqueda = this->libros->contains(unId);
-            
-            if (busqueda == NULL)
-            {
-                return "libro no encontrado"
-            }
-            
-            if (busqueda->habilitado == true)
-            {
-                return busqueda->titulo ++ " " ++ "H";
-            }
-            
-            if (busqueda->habilitado == false)
-            {
-                return busqueda->titulo ++ " " ++ "D";
-            }
-            
-        }
-
-        string cambiarHab(int unId)
-        {
-            Libro* busqueda = this->libros->contains(unId);
-            if (busqueda == NULL)
-            {
-                return "libro no encontrado"
-            }
-            
-            busqueda->habilitado = !(busqueda->habilitado);
-            
-            if (busqueda->habilitado == true)
-            {
-                this->cantHabilitados++;
-                return busqueda->titulo ++ " " ++ "H";
-            }
-
-            if (busqueda->habilitado == false)
-            {
-                this->cantHabilitados--;
-                return busqueda->titulo ++ " " ++ "D";
-            }
-        }
-
-        string totalLibros()
-        {
-            int cantDes = this->cantLibros - this->cantHabilitados;
-            return this->cantLibros ++ " " ++ this->cantHabilitados ++ " " ++ cantDes;
-        }
-
-
-    private:
-        int cantLibros;
-        int cantHabilitados;
-        AVL<Libro>* libros;
+    Libro(int unId): id(unId), titulo(" "), habilitado(true) {}
 };
 
 
-int main()
+class BibliotecaAVL
 {
-    return 0;
+private:
+    struct AVLNode
+    {
+        Libro libro;
+        AVLNode *left;
+        AVLNode *right;
+        int height;
+        AVLNode(Libro unLibro) : libro(unLibro), left(NULL), right(NULL), height(1) {}
+    };
+
+    AVLNode* root; // root of the tree
+    int cantLibros;
+    int cantHabilitados;
+
+    // A utility function to get the height of the tree.
+    // In case of an empty tree, it returns 0.
+    int height(AVLNode *node)
+    {
+        if (node == NULL)
+        {
+            return 0;
+        }
+        return node->height;
+    }
+
+    // A utility function to right
+    // rotate subtree rooted with y
+    AVLNode *rightRotate(AVLNode *y)
+    {
+        AVLNode *x = y->left;
+        AVLNode *T2 = x->right;
+
+        // Perform rotation
+        x->right = y;
+        y->left = T2;
+
+        // Update heights
+        y->height = std::max(height(y->left),
+                             height(y->right)) +
+                    1;
+        x->height = std::max(height(x->left),
+                             height(x->right)) +
+                    1;
+
+        // Return new root
+        return x;
+    }
+
+    // A utility function to left
+    // rotate subtree rooted with x
+    AVLNode *leftRotate(AVLNode *x)
+    {
+        AVLNode *y = x->right;
+        AVLNode *T2 = y->left;
+
+        // Perform rotation
+        y->left = x;
+        x->right = T2;
+
+        // Update heights
+        x->height = std::max(height(x->left),
+                             height(x->right)) +
+                    1;
+        y->height = std::max(height(y->left),
+                             height(y->right)) +
+                    1;
+
+        // Return new root
+        return y;
+    }
+
+    // Get Balance factor of node N
+    int getBalance(AVLNode *node)
+    {
+        if (node == NULL)
+            return 0;
+        return height(node->right) - height(node->left);
+    }
+
+    // Recursive function to insert a element
+    // in the subtree rooted with node and
+    // returns the new root of the subtree.
+    AVLNode *insert(AVLNode* node, Libro unLibro)
+    {
+        /* 1. Perform the normal BST insertion */
+        if (node == NULL)
+            return new AVLNode(unLibro);
+            this->cantHabilitados++;
+            this->cantLibros++;
+        if (unLibro.id < node->libro.id)
+            node->left = insert(node->left, unLibro);
+        else if (unLibro.id > node->libro.id)
+            node->right = insert(node->right, unLibro);
+        else{ // Equal elements are not allowed in BST
+            if (node->libro.habilitado == false)
+            {
+                cantHabilitados++;
+            }
+            node->libro = unLibro;
+            return node;
+        }
+
+        /* 2. Update height of this ancestor AVLnode */
+        node->height = 1 + std::max(height(node->left),
+                                    height(node->right));
+
+        /* 3. Get the balance factor of this ancestor
+            node to check whether this node became
+            unbalanced */
+        int balance = getBalance(node);
+
+        // If this node becomes unbalanced, then
+        // there are 4 cases
+
+        // Left Left Case
+        if (balance < -1 && unLibro.id < node->left->libro.id)
+            return rightRotate(node);
+
+        // Right Right Case
+        if (balance > 1 && unLibro.id > node->right->libro.id)
+            return leftRotate(node);
+
+        // Left Right Case
+        if (balance < -1 && unLibro.id > node->left->libro.id)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+        // Right Left Case
+        if (balance > 1 && unLibro.id < node->right->libro.id)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+        /* return the (unchanged) node pointer */
+        return node;
+    }
+
+    string contains(AVLNode *node, int unId) {
+        if(node == NULL) {
+            return "libro_no_encontrado";
+        }
+        if(node->libro.id == unId) {
+            return node->libro.titulo + " " + libroHabilitado(node->libro.habilitado);
+        }
+        else if(node->libro.id > node->libro.id) {
+            return contains(node->left, unId);
+        }
+        else {
+            return contains(node->right, unId);
+        }
+    }
+
+    string toogle(AVLNode *node, int unId) {
+        if(node == NULL) {
+            return "libro_no_encontrado";
+        }
+        if(node->libro.id == unId) {
+            node->libro.id = false;
+            return node->libro.titulo + " " + libroHabilitado(node->libro.habilitado);
+        }
+        else if(node->libro.id > node->libro.id) {
+            return toogle(node->left, unId);
+        }
+        else {
+            return toogle(node->right, unId);
+        }
+    }
+
+    string libroHabilitado(bool hab)
+    {
+        if (hab)
+        {
+            return "H";
+        }
+        else 
+        {
+            return "D";
+        }
+    }
+
+public:
+    BibliotecaAVL() : root(NULL), cantLibros(0), cantHabilitados(0) {}
+
+    void insert(int unId, string unNombre)
+    {
+        root = insert(root, Libro(unId, unNombre));
+    }
+
+    string contains(int unId)
+    {
+        return this->contains(root, unId);
+    }
+
+    string toogle(int unId)
+    {
+        return this->toogle(root, unId);
+    }
+
+    string totales()
+    {
+        int totalDesHab = this->cantLibros - this->cantHabilitados;
+        return std::to_string(this->cantLibros) + " " + std::to_string(this->cantHabilitados) + " " + std::to_string(totalDesHab);
+    }
+
+};
+
+int main(){
+
 }
