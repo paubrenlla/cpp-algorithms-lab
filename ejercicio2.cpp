e6f86cbdb20d396c9344061a354eed767e0cb3b7
paubrenlla <pau.brenlla@gmail.com>
2024-10-08 22:13:48 -0300

    feat: comienzo ejercicio2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,8 +5,44 @@
 
 using namespace std;
 
+class BibliotecaHash
+{
+private:
+    struct Libro
+    {
+        int id;
+        string titulo;
+        bool habilitado;
+
+        // Constructor
+        Libro(int unId, string unTitulo) : id(unId), titulo(unTitulo), habilitado(true) {}
+    };
+    int cantLibros;
+    int cantHabilitados;
+    int buckets;
+    Libro** libros;
+    //usaremoms V para vacio, E para eliminado y L para lleno
+    char* vacioElimLleno;
+
+public:
+    BibliotecaHash(int espereados)
+    {
+        this->cantLibros = 0;
+        this->cantHabilitados = 0;
+        this->buckets = espereados/0.7;
+        this->vacioElimLleno = new char[this->buckets];
+        this->libros* = new Libro[this->buckets];
+        
+        for (int i = 0; i < this->buckets; i++)
+        {
+            char[i] = 'V';
+            this->libros[i] = NULL;
+        }
+    }
+}
+
 int main()
 {
-    // TODO
+    
     return 0;
 }
