8990bcd1f5cc0ca681539b0f834979f5cb0442d0
paubrenlla <pau.brenlla@gmail.com>
2024-10-23 14:52:41 -0300

    feat: agregado de funcion potencia

--- funciones/enteros.cpp
+++ funciones/enteros.cpp
@@ -14,3 +14,12 @@ int max(int a, int b) {
 int min(int a, int b) {
     return a < b ? a : b;
 }
+
+int potencia(int base, int exponente) 
+{
+    int resultado = 1;
+    for (int i = 0; i < exponente; ++i) {
+        resultado *= base;
+    }
+    return resultado;
+}
