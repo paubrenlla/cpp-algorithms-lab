f15c76b042ebd6e767762bf14f3c66bb7a177b87
paubrenlla <pau.brenlla@gmail.com>
2024-10-29 15:57:30 -0300

    fix:solucion segmentation fault al entregar

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -19,10 +19,65 @@ private:
         Pedido(int unId, int unaPrioridad, bool llevar, string unTipo, int unaPos): id(unId), prioridad(unaPrioridad), paraLlevar(llevar), tipo(unTipo), posicion(unaPos) {}
     };
 
+    struct PedidoHash
+    {
+        Pedido* pedido;
+        PedidoHash* sig;
+
+        PedidoHash(Pedido* unPedido): pedido(unPedido), sig(NULL){}
+        PedidoHash(Pedido* unPedido, PedidoHash* lista): pedido(unPedido), sig(lista){}
+    };
+
     int ultimo;
     int size;
-    Pedido** ordenesAVL;
-    Pedido** modificaciones;
+    Pedido** ordenesBH;
+
+    PedidoHash** ordenesHash;
+    int buckets;
+    
+    int hash(int i)
+    {
+        return (i * 31) % this->buckets;
+    }
+
+    Pedido* findHash(int id)
+    {
+        int pos = this->hash(id);
+        PedidoHash* aux = this->ordenesHash[pos];
+        while (aux != NULL && aux->pedido->id != id)
+        {
+            aux = aux->sig;
+        }
+        return aux->pedido;
+    }
+
+    void agregarHash(Pedido* p)
+    {
+        int pos = this->hash(p->id);
+        
+        PedidoHash* aux = this->ordenesHash[pos];
+        this->ordenesHash[pos] = new PedidoHash(p, aux);
+    }
+
+    void borrarDeLista(int id, PedidoHash*& l) 
+    {
+        if (l != NULL) 
+        {
+            if (l->pedido->id == id) 
+            {
+                PedidoHash* aux = l;
+                l = l->sig;
+                delete aux;
+                aux = NULL;
+            }
+            else borrarDeLista(id, l->sig);
+        }
+    }
+
+    void eliminarHash(int id)
+    {
+        this->borrarDeLista(id, this->ordenesHash[this->hash(id)]);
+    }
 
     void flotar(int pos)
     {
@@ -41,32 +96,31 @@ private:
 
             pos = pos / 2;
         }
-        
     }
 
     int mejorPedido(int nodo1, int nodo2)
     {
-        if (this->ordenesAVL[nodo1]->prioridad > this->ordenesAVL[nodo2]->prioridad)
+        if (this->ordenesBH[nodo1]->prioridad < this->ordenesBH[nodo2]->prioridad)
         {
             return nodo1;
         } 
-        else if (this->ordenesAVL[nodo1]->prioridad < this->ordenesAVL[nodo2]->prioridad)
+        else if (this->ordenesBH[nodo1]->prioridad > this->ordenesBH[nodo2]->prioridad)
         {
             return nodo2;
         }
         else 
         {
-            if (this->ordenesAVL[nodo1]->paraLlevar && !this->ordenesAVL[nodo2]->paraLlevar)
+            if (this->ordenesBH[nodo1]->paraLlevar && !this->ordenesBH[nodo2]->paraLlevar)
             {
                 return nodo1;
             }
-            else if (this->ordenesAVL[nodo2]->paraLlevar && !this->ordenesAVL[nodo1]->paraLlevar)
+            else if (this->ordenesBH[nodo2]->paraLlevar && !this->ordenesBH[nodo1]->paraLlevar)
             {
                 return nodo2;
             }
             else
             {
-                if (this->ordenesAVL[nodo1]->id > this->ordenesAVL[nodo2]->id)
+                if (this->ordenesBH[nodo1]->id > this->ordenesBH[nodo2]->id)
                 {
                     return nodo2;
                 }
@@ -97,22 +151,24 @@ private:
 
     void swap(int i, int j)
     {
-        Pedido* temp = this->ordenesAVL[i];
+        Pedido* temp = this->ordenesBH[i];
 
-        this->ordenesAVL[i]->posicion = j;
-        this->ordenesAVL[i] = this->ordenesAVL[j];
+        this->ordenesBH[i]->posicion = j;
+        this->ordenesBH[i] = this->ordenesBH[j];
 
-        this->ordenesAVL[j]->posicion = i;
-        this->ordenesAVL[j] = temp;
+        this->ordenesBH[j]->posicion = i;
+        this->ordenesBH[j] = temp;
     }
 
-    void deleteUltimo()
+    int deleteUltimo()
     {
-        int id = this->ordenesAVL[this->ultimo]->id;
-        delete this->modificaciones[id];
-        this->modificaciones[id] = NULL;
-        this->ordenesAVL[this->ultimo] = NULL;
+        int id = this->ordenesBH[this->ultimo]->id;
+        this->eliminarHash(id);
+        
+        this->ordenesBH[this->ultimo] = NULL;
         this->ultimo--;
+        
+        return id;
     }
 
 public:
@@ -120,44 +176,61 @@ public:
     {
         this->size = unaCant + 1;
         this->ultimo = 0;
-        this->ordenesAVL = new Pedido*[this->size];
-        this->modificaciones = new Pedido*[this->size];
+        this->ordenesBH = new Pedido*[this->size];
+
+        this->buckets = (unaCant + 1) * 2; 
+        this->ordenesHash = new PedidoHash*[this->buckets];
 
         for (int i = 0; i < this->size; i++)
         {
-            this->ordenesAVL = NULL;
-            this->modificaciones = NULL;
+            this->ordenesBH[i] = NULL;
         }
+        for (int i = 0; i < this->buckets; i++)
+        {
+            this->ordenesHash[i] = NULL;
+        }
+        
     }
 
     void agregarPedido(int unId, int unaPrioridad, bool llevar, string unTipo)
     {
         this->ultimo = this->ultimo + 1;
 
-        this->ordenesAVL[this->ultimo] = new Pedido(unId, unaPrioridad, llevar, unTipo, this->ultimo);
-        this->modificaciones[unId] = this->ordenesAVL[this->ultimo];
+        this->ordenesBH[this->ultimo] = new Pedido(unId, unaPrioridad, llevar, unTipo, this->ultimo);
+        this->agregarHash(this->ordenesBH[this->ultimo]);
 
         this->flotar(this->ultimo);        
     }
 
     void cambiarLlevar(int id)
     {
-        this->modificaciones[id]->paraLlevar = true;
+        Pedido* aux = this->findHash(id);
+        aux->paraLlevar = true;
 
-        this->flotar(this->modificaciones[id]->posicion);
+        this->flotar(aux->posicion);
     }
 
     void entregar(int id)
     {
-        this->swap(this->ultimo, this->modificaciones[id]->posicion);
-        this->deleteUltimo();
+        this->swap(this->ultimo, this->findHash(id)->posicion);
+        int idElim = this->deleteUltimo();
 
-        this->hundir(this->modificaciones[id]->posicion);
+        if (this->esVacio() == false && idElim != id && this->ultimo > 1)
+        {
+            this->flotar(this->findHash(id)->posicion);
+            this->hundir(this->findHash(id)->posicion);
+        }
     }
 
     string pop()
     {
-        string retorno = std::to_string(this->ordenesAVL[1]->id) + " " + std::to_string(this->ordenesAVL[1]->prioridad) + " " + std::to_string(this->ordenesAVL[1]->paraLlevar) + " " + this->ordenesAVL[1]->tipo;
+        string llevar = "true";
+        if (this->ordenesBH[1]->paraLlevar == false)
+        {
+            llevar = "false";
+        }
+        
+        string retorno = std::to_string(this->ordenesBH[1]->id) + " " + std::to_string(this->ordenesBH[1]->prioridad) + " " + llevar + " " + this->ordenesBH[1]->tipo;
         
         this->swap(this->ultimo, 1);
         this->deleteUltimo();
@@ -182,30 +255,37 @@ int main()
 
     Pedidos* pedidos = new Pedidos(cantPedidos);
 
-    char command;
-    cin >> command;
+    string command;
+    
 
     for (int i = 0; i < cantOps; i++)
     {
-        if (command == 'I')
+        cin >> command;
+        if (command == "I")
         {
             int id;
             int prioridad;
-            bool paraLlevar;
+            string llevar;
+            bool paraLlevar = true;
             string tipo;
 
-            cin >> id >> prioridad >> paraLlevar >> tipo;
+            cin >> id >> prioridad >> llevar >> tipo;
+
+            if (llevar == "false")
+            {
+                paraLlevar = false;
+            }
 
             pedidos->agregarPedido(id, prioridad, paraLlevar, tipo);
         }
-        else if (command == 'C')
+        else if (command == "C")
         {
             int id;
             cin >> id;
 
             pedidos->cambiarLlevar(id);
         }
-        else if (command == 'E')
+        else if (command == "E")
         {
             int id;
             cin >> id;
@@ -219,6 +299,5 @@ int main()
         cout << pedidos->pop() << endl;
     }
     
-
     return 0;
 }
