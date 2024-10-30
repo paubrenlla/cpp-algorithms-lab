f34f6243f0152f509fcd8bd041cbb5872c7cb14c
Emiliano-Barboza <emiliano.barboza@gmail.com>
2024-10-29 23:11:12 -0300

    feature - ejercicio 3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,88 +1,35 @@
 #include <iostream>
 #include "./models/Item.cpp"
+#include "./tads/HeapSort.cpp"
 
 using namespace std;
 
-int getLeftChild(int parentIndex) {
-    return  2 * parentIndex + 1;
-}
-
-int getRightChild(int parentIndex) {
-    return  2 * parentIndex + 2;
-}
-
-void heapify(Item array[], int n, int i)
-{
-    Item temp;
-    int maximum, left_index, right_index;
-
-    // currently assuming i postion to
-    // be holding the largest value
-    maximum = i;
-
-    right_index = getRightChild(i);
-
-    left_index = getLeftChild(i);
-
-    // if left index value is grater than the current index
-    // value
-    if (left_index < n && array[left_index] > array[maximum]) {
-        maximum = left_index;
-    }
-    // if right index value is grater than the current index
-    // value
-    if (right_index < n && array[right_index] > array[maximum]) {
-        maximum = right_index;
-    }
-
-    // checking if we needed swaping the elements or not
-    if (maximum != i) {
-        temp = array[i];
-        array[i] = array[maximum];
-        array[maximum] = temp;
-        heapify(array, n, maximum);
-    }
-}
-
-void heapSort(Item array[], int size) {
-    Item temp;
-    for (int index = size / 2 - 1; index >= 0; index--) {
-        heapify(array, size, index);
-    }
-
-    for (int i = size - 1; i > 0; i--) {
-        temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
-        heapify(array, i, 0);
-    }
-}
-
-
 int main()
 {
-    int size, id, price;
+    int size, id, price, lowerPrices;
     cin >> size;
-    Item *items = new Item[size]();
+    HeapSort<Item> * maxHeap = new HeapSort<Item>(size);
 
     for (int i = 0; i < size; i++)
     {
         cin >> id;
         cin >> price;
         Item item(id, price);
-        items[i] = item;
+        maxHeap->insert(item);
     }
 
-    printf("Original Array : ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", items[i].id);
-    }
-
-    heapSort(items, size);
-
-    printf("Array after performing heap sort: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", items[i].id);
+    Item *items = maxHeap->sort();
+    cin >> lowerPrices;
+    int * ids = new int[size]();
+    int index = 0;
+    while (lowerPrices > 0) {
+        id = items[index].id;
+        if (ids[id] == 0) {
+            cout << items[index].id  << '\n';
+            ids[id] = 1;
+            lowerPrices--;
+        }
+        index++;
     }
 
     return 0;
--- tads/HeapSort.cpp
+++ tads/HeapSort.cpp
@@ -1,65 +1,104 @@
+#include <cassert>
+
+#include "Heap.h"
 #ifndef HEAP_SORT
 #define HEAP_SORT
-#include <string>
 using namespace std;
 
 template <class T>
-class HeapSort
-{
+class HeapSort: public Heap<T> {
 private:
+    int size{};
+    int lastElement;
+    T* elements;
+    bool isFull () {
+        return this->lastElement > this->size;
+    }
+
+    bool empty () {
+        return this->lastElement == 0;
+    }
+
+    int getParentIndex(int childIndex) {
+        return  (childIndex -1 ) / 2;
+    }
+
     int getLeftChild(int parentIndex) {
-      return  2 * parentIndex + 1;
+        return  2 * parentIndex + 1;
     }
 
     int getRightChild(int parentIndex) {
         return  2 * parentIndex + 2;
     }
 
-    void heapify(int array[], int n, int i)
+    void swap(int parentIndex, int childIndex) {
+        T aux = this->elements[parentIndex];
+        this->elements[parentIndex] = this->elements[childIndex];
+        this->elements[childIndex] = aux;
+    }
+
+    void bubbleUp(int index) {
+        if (index > 0) {
+            int parentIndex = this->getParentIndex(index);
+            T parent = this->elements[parentIndex];
+            T child = this->elements[index];
+            if (child > parent) {
+                this->swap(parentIndex, index);
+                this->bubbleUp(parentIndex);
+            }
+        }
+    }
+
+    void heapify(int size, int parentIndex)
     {
-        int temp, maximum, left_index, right_index;
+        int maximum = parentIndex;
+        int rightIndex = this->getRightChild(parentIndex);
+        int leftIndex = this->getLeftChild(parentIndex);
 
-        // currently assuming i postion to
-        // be holding the largest value
-        maximum = i;
-
-        right_index = this->getRightChild(i);
-
-        left_index = this->getLeftChild(i);
-
-        // if left index value is grater than the current index
-        // value
-        if (left_index < n && array[left_index] > array[maximum]) {
-            maximum = left_index;
-        }
-        // if right index value is grater than the current index
-        // value
-        if (right_index < n && array[right_index] > array[maximum]) {
-            maximum = right_index;
+        if (leftIndex < size && this->elements[leftIndex] > this->elements[maximum]) {
+            maximum = leftIndex;
         }
 
-        // checking if we needed swaping the elements or not
-        if (maximum != i) {
-            temp = array[i];
-            array[i] = array[maximum];
-            array[maximum] = temp;
-            heapify(array, n, maximum);
+        if (rightIndex < size && this->elements[rightIndex] > this->elements[maximum]) {
+            maximum = rightIndex;
+        }
+
+        if (maximum != parentIndex) {
+            this->swap(parentIndex, maximum);
+            heapify(size, maximum);
         }
     }
 
 public:
-    HeapSort(T array[], int size) {
-      int temp;
-        for (int index = size / 2 - 1; index >= 0; index--) {
-            heapify(array, size, index);
-        }
+    HeapSort(int size) {
+        this->elements = new T[size];
+        this->size = size;
+        this->lastElement = 0;
+    }
 
-        for (int i = size - 1; i > 0; i--) {
-            temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
-            heapify(array, i, 0);
+    void insert(T value) override {
+        assert(!this->isFull());
+        this->elements[this->lastElement] = value;
+        this->bubbleUp(this->lastElement);
+        ++this->lastElement;
+    };
+
+    T top() override {
+        assert(!this->empty());
+        return this->elements[0];
+    }
+
+    void remove() override {}
+
+    T* sort() {
+        assert(!this->empty());
+        for (int i = this->size - 1; i > 0; i--) {
+            T aux = this->elements[0];
+            this->elements[0] = this->elements[i];
+            this->elements[i] = aux;
+            heapify(i, 0);
         }
+        return this->elements;
     }
 };
 
