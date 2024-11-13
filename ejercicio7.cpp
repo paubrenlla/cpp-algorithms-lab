8e0b391d07cb41223c6778668caef06c22ad3cc2
paubrenlla <pau.brenlla@gmail.com>
2024-11-12 22:48:57 -0300

    feat: funciones basicas y read

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,12 +1,60 @@
 #include <cassert>
 #include <string>
 #include <iostream>
-#include <limits>
+#include <climits>
 
 using namespace std;
 
+class Ejercicio7
+{
+private:
+    
+    
+public:
+    Ejercicio7(int cantCanciones)
+    {
+
+    };
+
+    void add(int duracion)
+    {
+
+    }
+
+    void setAlumnos(int cantidad)
+    {
+
+    }
+
+    void procesar()
+    {
+
+    }
+
+};
+
+
 int main()
 {
-    // TODO
+    int cantCanciones = 0;
+    cin >> cantCanciones;
+
+    Ejercicio7* canciones = new Ejercicio7(cantCanciones);
+
+    for (int i = 0; i < cantCanciones; i++)
+    {
+        int duracion;
+        cin >> duracion;
+
+        canciones->add(duracion);
+    }
+
+    int cantAlumnos;
+    cin >> cantAlumnos;
+    
+    canciones->setAlumnos(cantAlumnos);
+
+    canciones->procesar();
+
     return 0;
 }
