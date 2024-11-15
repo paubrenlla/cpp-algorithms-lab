f225d942a2f424c44bdf0c0a1edd0307769b68e2
paubrenlla <pau.brenlla@gmail.com>
2024-11-15 12:47:28 -0300

    feat: tuple

---
+++ tads/Tuple.cpp
@@ -0,0 +1,27 @@
+#ifndef TUPLE
+#define TUPLE
+template <class A, class B>
+class Tuple
+{
+private:
+    A first;
+    B second;
+public:
+    Tuple() {}
+
+    Tuple(A first, B second) : first(first), second(second) {}
+
+    A getFirst() { return first; }
+
+    B getSecond() { return second; }
+
+    void setFirst(A first) { this->first = first; }
+
+    void setSecond(B second) { this->second = second; }
+
+    bool operator==(const Tuple<A, B> &other) const
+    {
+        return (first == other.first && second == other.second);
+    }
+};
+#endif
