17db3c7c1159cff7d6bde4821685de1b9d7ba118
paubrenlla <pau.brenlla@gmail.com>
2024-11-17 17:41:48 -0300

    fix: ejercicio6 listo

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -10,24 +10,36 @@ class Morfeo
 {
 private:
 
+    struct Conexion
+    {
+        int idDestino;
+        int costo;
+        Conexion* siguiente;
+
+        Conexion(int idDest, int cost) : idDestino(idDest), costo(cost), siguiente(NULL) {};
+        Conexion(int idDest, int cost, Conexion* sig) : idDestino(idDest), costo(cost), siguiente(sig) {};
+    };
+
     struct Ciudad
     {
-        int idCiudad;
-        string nombreCiudad;
+        int id;
+        string nombre;
+
+        int cantConexiones;
+        Conexion* conexiones;
         
-        Ciudad(int id, string nombre) : idCiudad(id), nombreCiudad(nombre) {};
+        Ciudad(int id, string nombre) : id(id), nombre(nombre), cantConexiones(0), conexiones(NULL) {};
     };
     
     int cantCiudades;
     Ciudad** ciudades;
-    int ** conexiones;
 
     int start;
     int entity;
     int team;
     int end;
     
-    int** dijkstra(int origen)
+    int** dijkstra(int origen, Ciudad** grafo)
     {
         bool *visitado = new bool[this->cantCiudades + 1]();
         int** costoVengo = new int*[this->cantCiudades + 1]();
@@ -53,19 +65,19 @@ private:
             {
                 visitado[top] = true;
 
-                for (int v = 1; v <= this->cantCiudades; v++)
+                Conexion* it = grafo[top]->conexiones;
+                while (it != NULL)
                 {
-                    if (this->conexiones[top][v] != -1)
+                    int v = it->idDestino;
+                    int peso = it->costo;
+                    
+                    if (!visitado[v] && costoVengo[v][0] > costoVengo[top][0] + peso)
                     {
-                        int peso = this->conexiones[top][v];
-                        
-                        if (!visitado[v] && costoVengo[v][0] > costoVengo[top][0] + peso)
-                        {
-                            costoVengo[v][0] = costoVengo[top][0] + peso;
-                            costoVengo[v][1] = top;
-                            cola->add(v, costoVengo[v][0]);
-                        }
+                        costoVengo[v][0] = costoVengo[top][0] + peso;
+                        costoVengo[v][1] = top;
+                        cola->add(v, costoVengo[v][0]);
                     }
+                    it = it->siguiente;
                 }
             }
         }
@@ -73,34 +85,76 @@ private:
         delete[] visitado;
         return costoVengo;       
     }
+
+    void actualizar(int origen, int destino, Ciudad**& grafo, int q)
+    {
+        if (q == 2)
+        {
+            return;
+        }
+
+        Conexion* aux = grafo[origen]->conexiones;
+        while (aux->idDestino != destino)
+        {
+            aux = aux->siguiente;
+        }
+        aux->costo = aux->costo * 2;
+
+        this->actualizar(destino, origen, grafo, q + 1);
+    }
     
-    string caminoRec(int** dijkstraResults, int v)
+    string caminoRec(int** dijkstraResults, int v, Ciudad**& grafo)
     {
         if (dijkstraResults[v][1] == -1)
         {
-            return this->ciudades[v]->nombreCiudad + " -> ";
+            return this->ciudades[v]->nombre + " -> ";
         }
         else
         {
-            return caminoRec(dijkstraResults, dijkstraResults[v][1]) + this->ciudades[v]->nombreCiudad + " -> ";
+            this->actualizar(v, dijkstraResults[v][1], grafo, 0);
+            return caminoRec(dijkstraResults, dijkstraResults[v][1], grafo) + this->ciudades[v]->nombre + " -> ";
         }
+    }
+
+    Ciudad** copiaCiudades()
+    {
+        Ciudad** copia = new Ciudad*[this->cantCiudades];
+        
+        for (int i = 1; i <= this->cantCiudades; i++)
+        {
+            copia[i] = new Ciudad(this->ciudades[i]->id, this->ciudades[i]->nombre);
+
+            Conexion* aux = this->ciudades[i]->conexiones;
+            Conexion* ultimo = NULL;
+            
+            while (aux != NULL)
+            {
+                Conexion* nuevaConexion = new Conexion(aux->idDestino, aux->costo);
+
+                if (ultimo == NULL)
+                {
+                    copia[i]->conexiones = nuevaConexion;
+                }
+                else
+                {
+                    ultimo->siguiente = nuevaConexion;
+                }
+                ultimo = nuevaConexion;
+                aux = aux->siguiente;
+            }
+            copia[i]->cantConexiones = this->ciudades[i]->cantConexiones;
+        }
+        return copia;
     }
 
 public:
     Morfeo(int cantCiudades)
     {
         this->ciudades = new Ciudad*[cantCiudades + 1];
-        this->conexiones = new int*[cantCiudades + 1];
 
         for (int i = 0; i <= cantCiudades; i++) 
         { 
             this->ciudades[i] = NULL;
-            this->conexiones[i] = new int[cantCiudades + 1];
-
-            for (int j = 0; j <= cantCiudades; j++) 
-            { 
-                this->conexiones[i][j] = -1;
-            }
         }
 
         this->cantCiudades = cantCiudades;
@@ -125,49 +179,72 @@ public:
 
     void addConexion(int origen, int destino, int costo)
     {
-        this->conexiones[origen][destino] = costo;
-        this->conexiones[destino][origen] = costo;
+        Conexion* aux = this->ciudades[origen]->conexiones;
+        this->ciudades[origen]->conexiones = new Conexion(destino, costo, aux);
+        this->ciudades[origen]->cantConexiones++;
     }
-
+    
     string procesar()
     {
-        int** dijkstraStart = this->dijkstra(this->start);
-        int** dijkstraEntity = this->dijkstra(this->entity);
-        int** dijkstraTeam = this->dijkstra(this->team);
+        //start-team-entity-end
+        Ciudad** step = copiaCiudades();
+        int sumaStep = 0;
 
-        //Opcion inicio-team-entity-end
-        int startTeam = dijkstraStart[this->team][0];
-        int teamEntity = dijkstraTeam[this->entity][0];
-        int entityEnd = 2 * dijkstraEntity[this->end][0];
+        int** dijkstraST = this->dijkstra(this->start, step);
+        sumaStep += dijkstraST[this->team][0];
+        string start_team = this->caminoRec(dijkstraST, this->team, step);
+        delete dijkstraST;
 
-        int suma1Team = startTeam + teamEntity + entityEnd;
+        int** dijkstraTE = this->dijkstra(this->team, step);
+        sumaStep += dijkstraTE[this->entity][0];
+        string team_entity = this->caminoRec(dijkstraTE, this->entity, step);
+        delete dijkstraTE;
 
-        //Opcion inicio-entity-team-end
-        int startEntity = dijkstraStart[this->entity][0];
-        int entityTeam = 2 * dijkstraEntity[this->team][0];
-        int teamEnd = 2 * dijkstraTeam[this->end][0];
+        int** dijkstraEP = this->dijkstra(this->entity, step);
+        sumaStep += dijkstraEP[this->end][0];
+        string entity_end = this->caminoRec(dijkstraEP, this->end, step);
+        delete dijkstraEP;
 
-        int suma1Entity = startEntity + entityTeam + teamEnd;
+        //start-entity-team-end
+        Ciudad** setp = copiaCiudades();
+        int sumaSetp = 0;
 
-        //eleccion camino
-        if (suma1Team < suma1Entity)
+        int** dijkstraSE = this->dijkstra(this->start, setp);
+        sumaSetp += dijkstraSE[this->entity][0];
+        string start_entity = this->caminoRec(dijkstraSE, this->entity, setp);
+        delete dijkstraSE;
+
+        int** dijkstraET = this->dijkstra(this->entity, setp);
+        sumaSetp += dijkstraET[this->team][0];
+        string entity_team = this->caminoRec(dijkstraET, this->team, setp);
+        delete dijkstraET;
+
+        int** dijkstraTP = this->dijkstra(this->team, setp);
+        sumaSetp += dijkstraTP[this->end][0];
+        string team_end = this->caminoRec(dijkstraTP, this->end, setp);
+        delete dijkstraTP;
+
+        if (sumaStep < sumaSetp)
         {
-            string descripcion = "BE11, la mejor ruta es buscar equipo, Desactivar la Entidad y punto de extraccion con un costo de " + std::to_string(suma1Team) + "\n";
-            string alt = "La otra opcion tiene un costo de " + std::to_string(suma1Entity) + "\n";
-            string start_team = "Paso 1: " + this->caminoRec(dijkstraStart, this->team) + "Buscar equipo \n";
-            string team_entity = "Paso 2: " + this->caminoRec(dijkstraTeam, this->entity) + "Desactivar la Entidad \n";
-            string entity_end = "Paso 3: " + this->caminoRec(dijkstraEntity, this->end) + "Ir a Punto de extraccion \n";
-            return descripcion + alt + start_team + team_entity + entity_end;
+            string descripcion = "BE11, la mejor ruta es buscar equipo, Desactivar la Entidad y punto de extraccion con un costo de " + std::to_string(sumaStep);
+            string alt = "La otra opcion tiene un costo de " + std::to_string(sumaSetp);
+            cout << descripcion << endl;
+            cout << alt << endl;
+            cout << "Paso 1: " +start_team + "Buscar equipo" << endl;
+            cout << "Paso 2: " + team_entity + "Desactivar la Entidad" << endl;
+            cout << "Paso 3: " + entity_end + "Ir a Punto de extraccion" << endl;
         }
-        if (suma1Entity < suma1Team)
+        else
         {
-            string descripcion = "BE11, la mejor ruta es Desactivar la Entidad, buscar equipo y punto de extraccion con un costo de " + std::to_string(suma1Entity) + "\n";
-            string alt = "La otra opcion tiene un costo de " + std::to_string(suma1Team) + "\n";
-            string start_entity = "Paso 1: " + this->caminoRec(dijkstraStart, this->entity) + "Desactivar la Entidad \n";
-            string entity_team = "Paso 2: " + this->caminoRec(dijkstraEntity, this->team) + "Buscar equipo \n";
-            string team_end = "Paso 3: " + this->caminoRec(dijkstraTeam, this->end) + "Ir a Punto de extraccion \n";
-            return descripcion + alt + start_entity + entity_team + team_end;
+            string descripcion = "BE11, la mejor ruta es Desactivar la Entidad, buscar equipo y punto de extraccion con un costo de " + std::to_string(sumaSetp);
+            string alt = "La otra opcion tiene un costo de " + std::to_string(sumaStep);
+            cout << descripcion << endl;
+            cout << alt << endl;
+            cout << "Paso 1: " + start_entity + "Desactivar la Entidad" << endl;
+            cout << "Paso 2: " + entity_team + "Buscar equipo" << endl;
+            cout << "Paso 3: " + team_end + "Ir a Punto de extraccion" << endl;
         }
+        
 
     }
 
@@ -184,12 +261,12 @@ int main()
 
     for (int i = 0; i < cantCiudades; i++)
     {
-        int idCiudad;
-        string nombreCiudad;
+        int id;
+        string nombre;
 
-        cin >> idCiudad >> nombreCiudad;
+        cin >> id >> nombre;
 
-        morfeo->addCiudad(idCiudad, nombreCiudad);
+        morfeo->addCiudad(id, nombre);
     }
 
     int comiento;
@@ -218,9 +295,10 @@ int main()
         cin >> idOrigen >> idDestino >> costo;
 
         morfeo->addConexion(idOrigen, idDestino, costo);
+        morfeo->addConexion(idDestino, idOrigen, costo);
     }
     
-    cout << morfeo->procesar() << endl;
+    morfeo->procesar();
 
     return 0;
 }
