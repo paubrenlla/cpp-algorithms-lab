4021c967e392485c7654178bdf5b41364eacfc77
paubrenlla <pau.brenlla@gmail.com>
2024-11-17 17:45:31 -0300

    feat: tests automaticos

---
+++ ejercicios.sh
@@ -0,0 +1,17 @@
+echo "Ejercicio 1"
+./ejercicio1.sh
+
+echo "Ejercicio 2"
+./ejercicio2.sh
+
+echo "Ejercicio 3"
+./ejercicio3.sh
+
+echo "Ejercicio 4"
+./ejercicio4.sh
+
+echo "Ejercicio 5"
+./ejercicio5.sh
+
+echo "Ejercicio 6"
+./ejercicio6.sh
